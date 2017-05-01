f180a52362d11e50d91bb3c232d51aa926233a1b
Justun Macharia Muriga <jm@refunite.org>
2017-05-01 09:11:44 +0000

    Version 1.1 of the simple python packaging test

---
+++ funniest/__init__.py
@@ -0,0 +1,2 @@
+def joke():
+    return(u'Can a kangaroo jump higher than a house? Of course, a house doesn\'t jump at all.')
---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup(name='jux-funniest-test',version='0.1',description='The funniest joke in the world',
+        url='https://github.com/machariajustun/simple-python-packaging-test',author='Justun Muriga',
+        author_email='machariajustun@gmail.com',license='MIT',
+        packages=['funniest'],zip_safe=False)
